98aa46fd864993d0def392ae14638dac5afed6ee
郭晨旭 <2269409349@qq.com>
2024-02-24 00:56:45 +0800

    增加日志

--- app/task/cmd/rpc/internal/logic/cache_logic.go
+++ app/task/cmd/rpc/internal/logic/cache_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"fmt"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 )
 
@@ -10,6 +11,7 @@ func DeleteListCache(r *redis.Redis, uid int64) {
 	prefix := "cache:gtodolist:task:list:"
 	listKey := fmt.Sprintf("%s%v:*", prefix, uid)
 	keys, _, _ := r.Scan(0, listKey, 0)
+	logx.Info("DeleteListCache, keys: ", keys)
 
 	for _, key := range keys {
 		_, _ = r.Del(key)
